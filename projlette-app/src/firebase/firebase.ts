4a28fa00b823035b6b125c26d699dfdf54734b5d
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2022-04-26 11:47:12 +0200

    Fix firebase

--- projlette-app/src/firebase/firebase.ts
+++ projlette-app/src/firebase/firebase.ts
@@ -1,18 +1,21 @@
-import * as firebase from "firebase/app";
+import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import * as auth from "firebase/auth";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.DEV_API_KEY,
-  authDomain: process.env.DEV_AUTH_DOMAIN,
-  databaseURL: process.env.DEV_DATABASE_URL,
-  projectId: process.env.DEV_PROJECT_ID,
-  storageBucket: process.env.DEV_STORAGE_BUCKET,
-  messagingSenderId: process.env.DEV_MESSAGING_SENDER_ID,
-  appId: process.env.DEV_APP_ID,
-  measurementId: process.env.DEV_MEASUREMENT_ID,
+  apiKey: process.env.REACT_APP_DEV_API_KEY,
+  authDomain: process.env.REACT_APP_DEV_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DEV_DATABASE_URL,
+  projectId: process.env.REACT_APP_DEV_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_DEV_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_DEV_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_DEV_APP_ID,
+  measurementId: process.env.REACT_APP_DEV_MEASUREMENT_ID,
 };
 
-const app = firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-export { analytics, app, auth, firebase };
+const auth = getAuth(app);
+const db = getFirestore(app);
+export { analytics, app, auth, db };
