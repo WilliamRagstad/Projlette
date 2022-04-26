d5cd254e0468ed5912830da1233b7c1bf0a2d7ad
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2022-04-26 15:59:23 +0200

    Update firebase.ts

--- projlette-app/src/firebase/firebase.ts
+++ projlette-app/src/firebase/firebase.ts
@@ -1,7 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getAuth, onAuthStateChanged, User } from "firebase/auth";
+import {
+  collection,
+  getDoc,
+  getDocs,
+  getFirestore,
+  query,
+  where,
+} from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_DEV_API_KEY,
@@ -19,3 +26,30 @@ const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const db = getFirestore(app);
 export { analytics, app, auth, db };
+
+export function getCurrentUser(): Promise<
+  Record<string, any> & { source: User }
+> {
+  return new Promise((resolve, reject) => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // Fetch user data from Firestore
+        const q = query(
+          collection(db, "users"),
+          where("email", "==", auth.currentUser.email),
+        );
+        getDocs(q).then((snap) => {
+          if (snap.docs.length === 1) {
+            resolve({ ...snap.docs[0].data(), source: user });
+          } else if (snap.docs.length > 1) {
+            reject(new Error("Multiple users with same email found"));
+          } else {
+            reject(new Error("No user found"));
+          }
+        });
+      } else {
+        reject(new Error("No user is currently signed in"));
+      }
+    });
+  });
+}
